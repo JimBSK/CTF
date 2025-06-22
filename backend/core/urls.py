0fec5066e4371d82f77dfca16e3274a1eb64ac50
Max <114328317+JimBSK@users.noreply.github.com>
2025-06-23 02:21:20 +0300

    urls updated

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('ctf.urls')), 
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('/adminpanel/admin/', admin.site.urls),
+    path('/adminpanel/api/', include('ctf.urls')), 
+    path('/adminpanel/api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/adminpanel/api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
